7acc5f7ffa4a938beb3ec32dd6059fcb53aaac9d
Huelton <hueltondihon@gmail.com>
2022-02-12 15:59:19 -0300

    Adicionando Classe Professor

---
+++ src/outros/com/citibank/aula4sabado/oo2/Professor.java
@@ -0,0 +1,22 @@
+package outros.com.citibank.aula4sabado.oo2;
+
+public class Professor {
+
+	private String nome;
+	private String sobreNome;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	
+	
+}
